e27458c717745306bbc742629d1360fbc19b8191
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2022-02-08 09:41:48 -0600

    Add files via upload

---
+++ busqueda secuencial.py
@@ -0,0 +1,46 @@
+#Importamos la libreria para generar numeros random
+from random import randint
+#Pedimos la longitud que va a tener la lista, para crearla
+longitud=int(input("Ingresa la longitud para el vector  "))
+#La funcion tiene un argumento, que sera la longitud del vector a crearlo
+def crear_vector(lon):
+    #Declaramos el vector, vacio
+    vec=[]
+    #Por cada vuelta en la longitud del vector
+    for i in range(lon):
+        #Generamos un numero random
+        x=randint(1,99)
+        #Agregamos el numero que generamos la final de la lista.
+        vec.append(x)
+    #Retornamos el vector
+    return vec
+#Asignamos el la respuesta de la funcion para crear, a una variable
+vector=crear_vector(longitud)
+#Pedimos un numero para buscar en la lista
+numero=int(input("Vector creado, ahora ingresa un numero para buscar "))
+#La funcion tiene dos parametros, el numero para buscar y el vector en donde va a buscar
+def busqueda(n,v):
+    #Declara una variable de tipo booleana, inciada en false
+    bandera=False
+    posicion=0
+    #Por cada vuelta en la longitud del vector en contexto
+    for i in range(len(v)):
+        #Si el numero a buscar, es igual al vector en la vuelta actual
+        if n==v[i]:
+            #Bandera cambia su valor a true
+            bandera=True
+            #La posicion es la vuelta actual mas uno, ya que las vueltas del for comienzan en 0 y quedaria una atras.
+            posicion=i+1
+    #Retorna un valor doble para descomponerlo despues
+    return bandera,posicion
+#Descompone la respuesta de la funcion en dos variables
+respuesta,pos=busqueda(numero,vector)
+#Si la respuesta es true, imprime que si se encuentra y tambien la posicion
+if respuesta:
+    print(f"El numero si se encuentra en la posicion {pos}")
+#Si no, dice que no se encuentra.
+else:
+    print("El numero no se encuentra en el vector")
+
+
+
