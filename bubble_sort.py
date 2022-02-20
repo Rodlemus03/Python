8b30827c25dc09c69a1d22466825b743dcdfbb96
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2022-02-19 21:56:45 -0800

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,25 @@
+from multiprocessing.context import BaseContext
+from operator import truediv
+from crear_vector import crear_vector
+v=crear_vector(10)
+#Version 1
+def bubble_sort(vector):
+    for i in range(0,len(vector)-1):
+        for j in range(0,len(vector)-1-i):
+            if(vector[j]>vector[j+1]):
+                vector[j],vector[j+1]=vector[j+1],vector[j]
+    return vector
+#Version 2
+def bubble_sort_mejorado(vector):
+    i=0
+    bandera=True
+    while(i<=len(vector)-2) and bandera:
+        bandera=False
+        for j in range(0,len(vector)-i-1):
+            if(vector[j]>vector[j+1]):
+                vector[j],vector[j+1]=vector[j+1],vector[j]
+                bandera=True
+        i+=1
+    return vector
+
+
